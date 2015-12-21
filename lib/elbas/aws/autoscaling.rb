37f65cf414e1beba7b2f2be5808514b3d5ff071f
Gautam Sathe <gautam@hmsnz.com>
2015-12-22 09:32:47 +1300

    Fix condition bug in retrieving hostname
    Signed-off-by: Gautam Sathe <gautam@hmsnz.com>

--- lib/elbas/aws/autoscaling.rb
+++ lib/elbas/aws/autoscaling.rb
@@ -20,7 +20,6 @@ module Elbas
       def autoscale_group_name
         fetch(:aws_autoscale_group)
       end
-
     end
   end
 end
--- lib/elbas/capistrano.rb
+++ lib/elbas/capistrano.rb
@@ -13,9 +13,14 @@ def autoscale(groupname, *args)
   set :aws_autoscale_group, groupname
 
   running_instances.each do |instance|
-    hostname = instance.dns_name || instance.private_ip_address
+    hostname = if instance.dns_name && !instance.dns_name.empty?
+                 instance.dns_name
+               else
+                 instance.private_ip_address
+               end
+    # hostname = instance.dns_name || instance.private_ip_address
     p "ELBAS: Adding server: #{hostname}"
-    server(hostname, *args)
+    server(hostname, *args) if hostname && !hostname.empty?
   end
 
   if running_instances.count > 0
